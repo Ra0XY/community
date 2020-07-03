e64617876571a5aeadb9073c2c2ddb222738d53a
Ra0XY <1425245866@qq.com>
2020-07-03 10:30:49 +0800

    init repo

---
+++ src/test/java/com/rao/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rao.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
